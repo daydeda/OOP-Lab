b63dd1c99d01f1df89a1eff8467ed6d7c22099ae
daydedaa <daydedaa@gmail.com>
2026-01-08 10:14:41 +0700

    Add UML .png

--- LAB06_Inheritance/Person/Faculty.java
+++ LAB06_Inheritance/Person/Faculty.java
@@ -2,7 +2,8 @@ public class Faculty extends Employee {
     private String officeHours;
     private String rank;
 
-    public Faculty(String name, String address, String phoneNumber, String email, String office, double salary, String officeHours, String rank) {
+    public Faculty(String name, String address, String phoneNumber, String email, String office, double salary,
+            String officeHours, String rank) {
         super(name, address, phoneNumber, email, office, salary);
         this.officeHours = officeHours;
         this.rank = rank;
