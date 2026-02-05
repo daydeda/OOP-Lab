ac093e55492618b09ae6915fa3e13d1ed6ace99b
daydedaa <daydedaa@gmail.com>
2026-02-05 09:30:04 +0700

    LAB07 Fix .gitignore and remove tracked class files

--- LAB03_OOThinking/Class_Relationships/TestAggregation.java
+++
@@ -1,9 +0,0 @@
-package Class_Relationships;
-
-public class TestAggregation {
-    public static void main(String[] args){
-        Address address = new Address("123 Main St", "Chiang Mai", "50200");
-        Student student = new Student("682115036", "Puri Sangnawakit", address);
-        System.out.println(student.getStudentInfo());
-    }
-}
